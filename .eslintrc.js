495cbd776304f102a1d41570801d5f97571f24da
fishineyuan <382184760@qq.com>
2017-01-17 01:38:11 +0800

    fix error fix help

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,33 @@
+
 module.exports = {
     "extends": "airbnb",
     "installedESLint": true,
+    "env": {
+        "browser": true,
+        "node": true
+    },
     "plugins": [
         "react",
         "jsx-a11y",
         "import"
     ],
     "rules": {
-        "indent": ["error", 4]
+        "indent": ["error", 4],
+        "comma-dangle": "warn",
+        "no-underscore-dangle": "off",
+        "class-methods-use-this": "warn",
+        "no-unused-expressions": "warn",
+        "one-var": ["warn", "always"],
+        "arrow-body-style": ["warn", "as-needed"],
+        "space-before-function-paren": ["error", "never"],
+        "no-trailing-spaces": ["error", { "skipBlankLines": true }],
+        "import/no-absolute-path": "off",
+        "import/extensions": "off",
+        "import/no-unresolved": "off",
+        "import/no-named-as-default-member": "off",
+        "import/no-named-as-default": "off",
+        "react/prop-types": "off",
+        "react/jsx-filename-extension": "off",
+        "react/jsx-indent": ["error", 4]
     }
 };
